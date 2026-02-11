1fba374c1c9e4db7b223dcb4bcf9009cf0e18f7c
Justin Shaw <shaw.justin@shaw.ca>
2026-02-11 15:50:18 -0700

    add try catch block around web listener

--- server/main.py
+++ server/main.py
@@ -23,7 +23,7 @@ PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
 # Set descriptive variables so we don't have to remember what the god numbers mean
 timed_max_pings = 3 # per second max
-total_max_pings = 10 # total transactions
+total_max_pings = 6 # total transactions
 
 # Set up counters
 ping_count_timed = Counter()
@@ -47,27 +47,29 @@ def thread_function(evt):
 # Start Timer
 thread = Thread(target=thread_function, args=(Event(),))
 thread.start()
+try:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"Server listening on {HOST}:{PORT}")
+        conn, addr = s.accept()
+        with conn:
+            print(f'Connected by {addr}')
+            while True:
+                data = conn.recv(1024)
+                print(f"Received from client: {data.decode()}")
+                response = b"Pong"
+                conn.sendall(response)
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server listening on {HOST}:{PORT}")
-    conn, addr = s.accept()
-    with conn:
-        print(f'Connected by {addr}')
-        while True:
-            data = conn.recv(1024)
-            print(f"Received from client: {data.decode()}")
-            response = b"Pong"
-            conn.sendall(response)
-
-            ping_count_timed.increment()
-            ping_count_total.increment()
-            if ping_count_total.get_count() > total_max_pings:
-                print("SCAMMER GET SCAMMED")
-                break
-            elif ping_count_timed.get_count() >= timed_max_pings:
-                print("SCAMMER GET SCAMMED")
-                break
-            else:
-                continue
+                ping_count_timed.increment()
+                ping_count_total.increment()
+                if ping_count_total.get_count() >= total_max_pings:
+                    print("SCAMMER GET SCAMMED")
+                    break
+                elif ping_count_timed.get_count() >= timed_max_pings:
+                    print("SCAMMER GET SCAMMED")
+                    break
+                else:
+                    continue
+except ConnectionResetError:
+    print(f"Failed to connect to listening port {PORT}")
