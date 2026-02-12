2d63b99753dfec991a918ec69da5c31a25780eb6
Justin Shaw <shaw.justin@shaw.ca>
2026-02-11 17:01:32 -0700

    add client side tests

--- client/main.py
+++ client/main.py
@@ -1,27 +1,35 @@
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
-PORT = 65432        # The port used by the server
-
-def new_ping(message) -> None:
-    s.sendall(message.encode())
-    data = s.recv(1024)
+def new_ping(message, sock):
+    sock.sendall(message.encode())
+    data = sock.recv(1024)
     print(f"Received from server: {data.decode()}")
+    return data
 
-try:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST, PORT))
-        print(f"Connected to server {HOST}:{PORT}")
-        while True:
-            print("What do?")
-            user_input = input()
-            if user_input == "ping":
-                new_ping("Buy Ticket")
-                continue
-            elif user_input == "ping many":
-                for i in range(7):
-                    new_ping(f"Buy Ticket #{i+1}")
-                continue
+def main():
+    HOST = '127.0.0.1'  # The server's hostname or IP address
+    PORT = 65432        # The port used by the server
 
-except ConnectionResetError:
-    print(f"Failed to connect to listening port {PORT}")
+    try:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.connect((HOST, PORT))
+            print(f"Connected to server {HOST}:{PORT}")
+            while True:
+                print("What would you like to do?")
+                user_input = input()
+                if user_input == "ping":
+                    new_ping("Buy Ticket", s)
+                    continue
+                elif user_input == "ping many":
+                    for i in range(7):
+                        new_ping(f"Buy Ticket #{i+1}", s)
+                    continue
+                elif user_input == "kill":
+                    print("Killing Connection")
+                    break
+
+    except ConnectionResetError:
+        print(f"Failed to connect to listening port {PORT}")
+
+if __name__ == "__main__":
+    main()
---
+++ client/tests.py
@@ -0,0 +1,14 @@
+import unittest
+from unittest.mock import MagicMock
+from main import new_ping
+
+class TestPings(unittest.TestCase):
+    def test_ping(self):
+        mock_socket = MagicMock()
+
+        mock_socket.recv.return_value = b"PONG"
+        result = new_ping("PING", mock_socket)
+
+        mock_socket.sendall.assert_called_once_with(b"PING")
+        mock_socket.recv.assert_called_once_with(1024)
+        self.assertEqual(result, b"PONG")
