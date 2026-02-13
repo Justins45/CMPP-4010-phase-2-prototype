6f6d5736aaa3570c89d5232740c24db8d799247b
Justin Shaw <shaw.justin@shaw.ca>
2026-02-12 17:09:03 -0700

    add back class but better!!

--- server/main.py
+++ server/main.py
@@ -2,6 +2,36 @@ import socket
 import threading
 from time import sleep
 
+class Connector:
+
+    def __init__(self):
+        self.IP_ADDRESS = ""
+        self.timed_count = 0
+        self.total_count = 0
+
+    def set_ip(self, new_ip):
+        self.IP_ADDRESS = new_ip
+
+    def get_ip(self):
+        return self.IP_ADDRESS
+
+    def increment_count(self):
+        self.timed_count += 1
+        self.total_count += 1
+
+    def decrement_timed(self):
+        self.timed_count -= 1
+
+    def reset_timed(self):
+        self.timed_count = 0
+
+    def get_timed_count(self):
+        return self.timed_count
+
+    def get_total_count(self):
+        return self.total_count
+
+
 # Configuration
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -11,8 +41,7 @@ timed_max_pings = 3  # per second max
 total_max_pings = 6  # total transactions
 
 # Data Structures
-# This dictionary tracks IPs: { '127.0.0.1': {'timed_count': 0, 'total_count': 0, 'banned': False} }
-client_data = {}
+client_data = Connector()
 data_lock = threading.Lock()
 
 
@@ -23,26 +52,15 @@ def decrease_counters():
     while True:
         sleep(1)
         with data_lock:
-            #Loop through all known IP addresses and lower their timed count
-            for ip in client_data:
-                if client_data[ip]['timed_count'] > 0:
-                    client_data[ip]['timed_count'] -= 1
+            # Lower times count
+            if client_data.get_timed_count() > 0:
+                client_data.decrement_timed()
 
 
 def handle_client(conn, addr):
     ip_address = addr[0]
+    client_data.set_ip(addr[0])
     print(f'Connected by {addr}')
-
-    #Initialize this IP in our database if new
-    with data_lock:
-        if ip_address not in client_data:
-            client_data[ip_address] = {'timed_count': 0, 'total_count': 0, 'banned': False}
-
-    #Check if they are already banned before talking
-    if client_data[ip_address]['banned']:
-        print("SCAMMER GET SCAMMED (Connection Refused)")
-        conn.close()
-        return
 
     with conn:
         while True:
@@ -57,27 +75,21 @@ def handle_client(conn, addr):
 
                 with data_lock:
                     #Increment counts
-                    client_data[ip_address]['timed_count'] += 1
-                    client_data[ip_address]['total_count'] += 1
+                    client_data.increment_count()
 
-                    #Check Thresholds
-                    current_timed = client_data[ip_address]['timed_count']
-                    current_total = client_data[ip_address]['total_count']
-
-                    if current_total >= total_max_pings:
+                    if client_data.get_total_count() >= total_max_pings:
                         print("SCAMMER GET SCAMMED (Total Limit)")
-                        client_data[ip_address]['banned'] = True
                         should_ban = True
 
-                    elif current_timed >= timed_max_pings:
+                    elif client_data.get_timed_count() >= timed_max_pings:
                         print("SCAMMER GET SCAMMED (Speed Limit)")
-                        client_data[ip_address]['banned'] = True
                         should_ban = True
 
                 if should_ban:
                     #Kill the connection from the server
                     error_msg = "SCAMMER GET SCAMMED"
                     conn.sendall(error_msg.encode())
+                    # conn.fuckoff
                     break
                 else:
                     response = b"Pong"
@@ -86,24 +98,20 @@ def handle_client(conn, addr):
             except ConnectionResetError:
                 break
 
-    # print(f"Connection closed for {ip_address}")
-
 
 def main():
-    #Start the imer
+    # Start the imer
     timer_thread = threading.Thread(target=decrease_counters, daemon=True) #Daemon=True makes it so when the main thread ends
-    #This specific thread dies immediately along with it.
+    # This specific thread dies immediately along with it.
     timer_thread.start()
 
     print(f"Server listening on {HOST}:{PORT}")
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        #This line helps avoid "Address already in use" errors if you restart quickly
+        # This line helps avoid "Address already in use" errors if you restart quickly
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
-        #s.fuckoff
-
 
         while True:
             conn, addr = s.accept()
