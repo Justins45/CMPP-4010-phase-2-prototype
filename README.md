df39d11cce6f7cf6938b7aeea9a2b7eff9b64bd6
Justin Shaw <shaw.justin@shaw.ca>
2026-04-10 14:49:00 -0600

    Update run_program.bat file to account for Docker Compos

--- README.md
+++ README.md
@@ -4,12 +4,13 @@ The idea behind this prototype is to showcase how it can be possible
 for software to exist that accurately prevents ticket scammers from 
 getting too many tickets with the intent to resell. 
 
-There are a few ways to run this project:
+## Prerequisite
 
-1. Utilize built Python itself to run `/client/main.py` and `/server/main.py` files in separate terminals
-2. Utilize Windows `run_program.bat` and `run_tests.bat` files (windows only at the moment)
-3. Utilize Docker Compose:
-   - Start containers in the background `docker-compose up --build -d`
-   - Terminal A run `docker-compose logs -f server`
-   - Terminal B run `docker-compose exec client python3 -u main.py`
-   - ps. (don't forget `docker-compose down`)
+1. Python Installed on your machine
+2. Docker Desktop installed and running (background or foreground)
+3. Open `Port 8080` (localhost)
+
+## Run the Program
+
+To Run the program use the `run_program.bat` file
+
--- run_program.bat
+++ run_program.bat
@@ -1,5 +1,37 @@
 @echo off
 @REM This Script runs the program (server + client) python files
-start cmd /k python3 server/main.py
-start cmd /k python3 client/main.py
+
+@REM set local directory as working directory
+cd /d %~dp0 
+
+@REM --- Check if Docker is running ---
+echo Checking Docker status...
+docker info >nul 2>&1
+IF %ERRORLEVEL% NEQ 0 (
+    echo.
+    echo Docker does NOT appear to be running.
+    echo Please start Docker Desktop and try again.
+    pause
+    exit /b 1
+) ELSE (
+    echo Docker is running. Proceeding with setup...
+)
+
+
+@REM Build docker containers and run in background
+@REM && runs second command ONLY if the first command is successful
+docker-compose build --no-cache && docker-compose up -d 
+
+@REM start is to open the command in a new terminal 
+
+start "SERVER_LOGS" cmd /k docker-compose logs -f server
+start "CLIENT_LOGS" cmd /k docker-compose exec client python3 -u main.py
+
+@REM Pause terminal and allow user to shut it all down when they are done.
+echo.
+echo Press any key to shut everything down...
+pause >nul
+
+echo Stopping containers...
+docker-compose down
 exit
